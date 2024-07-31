d67e4bff2339b10b31166ed27be5cdad6d370e84
bharathraj25 <bharathrajgngowdara@gmail.com>
2024-08-01 05:23:13 +0800

    fix: update config file

--- backendSystem/src/config.js
+++ backendSystem/src/config.js
@@ -5,11 +5,16 @@ const DOMAIN = process.env.DOMAIN || "localhost";
 const REDIS_PORT = process.env.REDIS_PORT || "6379";
 const REDIS_HOST = process.env.REDIS_HOST || "localhost";
 const etherscanApiKey = process.env.ETHERSCAN_API_KEY;
+const mo = process.env.ETHERSCAN_API_KEY;
+const moralisAppId = process.env.MORALIS_APP_ID;
+const ETHERSCAN_MAX_WINDOW = 10000;
 
 module.exports = {
   PORT,
   DOMAIN,
   REDIS_PORT,
   REDIS_HOST,
-  etherscanApiKey
+  etherscanApiKey,
+  moralisAppId,
+  ETHERSCAN_MAX_WINDOW,
 };
