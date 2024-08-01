75c453282fcf2bea211f6b2a002c670d4a421fb5
bharathraj25 <bharathrajgngowdara@gmail.com>
2024-08-01 15:11:15 +0800

    feat: add exchange price route

--- backendSystem/src/routes/api/index.js
+++ backendSystem/src/routes/api/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const jobRoute = require("./job");
+const exchangePriceRoute = require("./exchangePrice");
 
 router.use("/job", jobRoute);
+router.use("/exchangePrice", exchangePriceRoute);
 
 module.exports = router;
